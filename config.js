6e5bbdfb0a8d72dd52bac33dab06440fd4acd8c3
decofcp2-afk <decof.cp2@gmail.com>
2026-06-15 16:57:18 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.PAINEL_CONFIG = {
-  // URL /exec da implantacao Web App do Apps Script do PAINEL das Licitacoes Compartilhadas.
-  // Cole aqui a URL terminada em /exec do deploy ligado a planilha das licitacoes compartilhadas.
-  apiUrl: "COLE_AQUI_A_URL_EXEC_DO_APPS_SCRIPT_DO_PAINEL_COMPARTILHADA/exec"
+  // URL /exec do Apps Script do PAINEL das Licitacoes Compartilhadas (somente leitura).
+  apiUrl: "https://script.google.com/macros/s/AKfycbxlyWAVq6ShsO2QPdfG9_i0xDtZVqI33QvpDdrQdzJnnT_tDcFrzh2mZ7OVePFHlm4u_A/exec"
 };
